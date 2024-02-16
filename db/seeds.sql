fe00410dff10948cbb7dc23f0285aca61a8a8688
Arya Saini <arya30012@gmail.com>
2024-02-16 16:05:59 -0500

    functioning employee tracker

---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (name)
+VALUES 
+       ("Tech"),
+       ("Healthcare"),
+       ("HR"),
+       ("Security");
+
+INSERT INTO role (title, salary)
+VALUES 
+       ("Programmer", 20 ),
+       ("Doctor", 30),
+       ("Guidance Counselor", 15),
+       ("Security Guard", 10);
+
+INSERT INTO employee (first_name, last_name)
+VALUES 
+       ("Dave", "Ross"),
+       ("Bob", "Smith"),
+       ("Amber", "Rose"),
+       ("Sally", "Heart");
+
+
+       
+     
+       
